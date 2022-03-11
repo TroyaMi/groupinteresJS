281cbcc94239702c43c8138d7310e3b12895d677
Miguel Troya <miguel.troyat@unac.edu.co>
2022-03-11 13:34:05 -0500

    hdhd

--- prueba.js
+++ prueba.js
@@ -1,21 +1,23 @@
-consola _ log ( 'Ahora si es verdad que estoy programando' )
+console.log ( 'Ahora si es verdad que estoy programando' )
 
 //var nombre = 'Miguel'
 //var apellido = 'Troya'
 
 //var nombreCompleto = nombre + apellido
-//consola.log(nombreCompleto)
+//console.log(nombreCompleto)
 
 //console.log('Tu nombre completo es:' + nombreCompleto)
 
 //alert('Tu nombre completo es:' + nombreCompleto)
 
-//var nombre = prompt('Digite su nombre:')
-//var estadoCivil = prompt('Digita tu estado civil:')
+var nombre = prompt('Digite su nombre:')
+var estadoCivil = prompt('Digita tu estado civil:')
 
-//alerta('Nombre: ' + nombre + ' de estado civil: ' + estadoCivil)
+alert('Nombre: ' + nombre + ' de estado civil: ' + estadoCivil)
 
-var  number1  =  indicador ( 'Digite un numero' )
-var  number2  =  prompt ( 'Digite un segundo numero' )
-var  suma  =  parseInt ( número1 )  +  parseInt ( número2 )
-alerta ( suma )
+var number = prompt('Digite un numero')
+var number2 = prompt('Digite un segundo numero')
+
+var sumar = parseInt (number) + parseInt(number2)
+
+alert (sumar)
